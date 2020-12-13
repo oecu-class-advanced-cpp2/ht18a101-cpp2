0010f8412cf27a387764d01beedd69f0e7b14cba
k <k>
2020-12-13 19:05:10 +0900

    report2

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限値。
+using namespace std;
+#define CPP2_PRIME_UPPER_LIMIT 1000000 //探索する値の上限
+
 /* --------------------------------------------------------------- */
 /*
 * nth_prime
@@ -11,9 +13,59 @@
 * ファイルを作成せよ。
 */
 /* -------------------------------------------------------------- */
-int nth_prime(unsigned int a, unsigned int d, unsigned int n);
+bool is_prime(int num) {
+	int count = 0;
+	if (num <= 1) {
+		return false;
+	}
+	for (int a = 2;a <= num - 1;a++) {
+		if (num % a == 0) {
+			return false;
+		}
+	}
+		return true;
+}
+
+int nth_prime(unsigned int a, unsigned int d, unsigned int n) {
+	int current = a;
+	int count = 0;
+	while (current<=CPP2_PRIME_UPPER_LIMIT) {
+		if (is_prime(current)) {
+			count++;
+			if (count == n) {
+				return current;
+			}
+		}
+		current += d;
+	}
+	return false;
+}
+
 int main() {
- std::cout << nth_prime(367, 186, 151) << std::endl;
- // 以下、同様に、入出力例通りになるか確認せよ。
-return 0;
-} 
+	// 以下、同様に、入出力例通りになるか確認せよ。
+	cout<<"入力: a:367,d:186,n:151 出力: "<<nth_prime(367, 186, 151)<<endl;
+	
+	cout <<"入力: a:179,d:10,n:203 出力: "<< nth_prime(179, 10, 203) << endl;
+	
+	cout <<"入力: a:271,d:37,n:39 出力: "<<nth_prime(271, 37, 39) << endl;
+	
+	cout <<"入力: a:103,d:230,n:1 出力: "<<nth_prime(103, 230, 1) << endl;
+	
+	cout <<"入力: a:27,d:104,n:185 出力: " << nth_prime(27, 104, 185) << endl;
+	
+	cout <<"入力: a:253,d:50,n:85 出力: " << nth_prime(253, 50, 85) << endl;
+	
+	cout <<"入力: a:1,d:1,n:1 出力: " << nth_prime(1, 1, 1) << endl;
+	
+	cout <<"入力: a:9075,d:337,n:210 出力: " << nth_prime(9075, 337, 210) << endl;
+	
+	cout <<"入力: a:307,d:24,n:79 出力: "<< nth_prime(307, 24, 79) << endl;
+	
+	cout <<"入力: a:331,d:221,n:177 出力: "<< nth_prime(331, 221, 177) << endl;
+	
+	cout <<"入力: a:259,d:170,n:40 出力: "<< nth_prime(259, 170, 40) << endl;
+	
+	cout <<"入力: a:269,d:58,n:102 出力: " << nth_prime(269, 58, 102) << endl;
+	
+	return 0;
+}
